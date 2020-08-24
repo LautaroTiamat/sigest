10766ed92fb4336321f6124623a64bc15a5b5801
Rolón Lautaro Emanuel <61741642+LautaroTiamat@users.noreply.github.com>
2020-08-24 08:31:17 -0300

    Configuración

--- includes/config/connection.php
+++ includes/config/connection.php
@@ -1,8 +1,8 @@
 <?php
-$dbhost = 'localhost';
-$dbuser = 'root';
-$dbpass = 'lupita123';
-$dbname = 'sigest';
+$dbhost = getenv('VAR_HOST');
+$dbuser = getenv('VAR_USER');
+$dbpass = getenv('VAR_PASS');
+$dbname = getenv('VAR_DB');
 
 try {
 	$conexion = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
